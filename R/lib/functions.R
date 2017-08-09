c30d386ea6c6fbd58464200dd4efb30607a58cdc
Sylvio Campos Neto <s.camposn@gmail.com>
2017-08-09 16:58:54 -0300

    web release version

--- R/lib/functions.R
+++ R/lib/functions.R
@@ -170,3 +170,141 @@ cdb_mktMkt_bo <- function(day, bo_santander){
     collect() -> bo_san
   return(bo_san)
 }
+
+share_san <- function(tbl_mercado_cdb,tbl_santander_cdb,tbl_mercado_oc,tbl_santander_oc,bo_santander){
+  tbl_mercado_cdb %>% 
+    filter(Peer == "Grandes bancos de Varejo",
+           INDICE == "DI",
+           TICKET_AJUSTADO %in% c("(H) De 10 a 20 milhões","(I) De 20 a 50 milhões", "(J) De 50 a 100 milhões","(K) Mais de 100 milhões"),
+           EMISSOR == "Mercado",
+           DETENTOR == "Cliente",
+           DAT_REGISTRO >= '2016-12-02') %>% 
+    collect() %>% 
+    unite(COD,CONDICAO_RESGATE,CONDICAO_ESPECIFICA,sep="") %>% 
+    filter(COD %in% c("CONDICAO A MERCADONAO","TEM CONDICAOSIM")) %>%
+    group_by(DAT_REGISTRO) %>% 
+    summarise(VOL_FIN = sum(VAL_FINANCEIRO_TOTAL)) -> full_mkt
+  
+  tbl_santander_cdb %>% 
+    filter(Peer == "Grandes bancos de Varejo",
+           INDICE == "DI",
+           TICKET_AJUSTADO %in% c("(H) De 10 a 20 milhões","(I) De 20 a 50 milhões", "(J) De 50 a 100 milhões","(K) Mais de 100 milhões"),
+           EMISSOR == "Mercado",
+           DETENTOR == "Cliente",
+           DAT_REGISTRO >= '2016-12-02') %>% 
+    collect() %>%
+    unite(COD,CONDICAO_RESGATE,CONDICAO_ESPECIFICA,sep="") %>% 
+    filter(COD %in% c("CONDICAO A MERCADONAO","TEM CONDICAOSIM")) %>% 
+    group_by(DAT_REGISTRO) %>% 
+    summarise(VOL_FIN = sum(VAL_FINANCEIRO_TOTAL)) -> cdb_san
+  
+  full_mkt %>% 
+    full_join(cdb_san, by='DAT_REGISTRO') %>% 
+    mutate(VOL_CDB = VOL_FIN.x - VOL_FIN.y) %>% 
+    select(DAT_REGISTRO,VOL_CDB) %>% 
+    collect() -> cdb_mkt
+  
+  tbl_mercado_oc %>% 
+    filter(TICKET %in% c("(H) De 10 a 20 milhões","(I) De 20 a 50 milhões", "(J) De 50 a 100 milhões","(K) Mais de 100 milhões"),
+           ATIVO == 'DEB',
+           INDEXADOR == 'DI',
+           TIPO_ATIVO == 'Leasing',
+           PEER == 'Grandes bancos de Varejo') %>%
+    mutate(VOL = VAL_FINANCEIRO, Tx = VAL_FINANCEIRO * TAXA_MEDIA) %>% 
+    group_by(DAT_REGISTRO) %>% 
+    summarise(VOL_OC = sum(VOL)) %>% 
+    collect() -> full_oc_prop_mkt
+  
+  tbl_santander_oc %>% 
+    filter(TICKET %in% c("(H) De 10 a 20 milhões","(I) De 20 a 50 milhões", "(J) De 50 a 100 milhões","(K) Mais de 100 milhões"),
+           ATIVO == 'DEB',
+           INDEXADOR == 'DI',
+           TIPO_ATIVO == 'Leasing',
+           PEER == 'Grandes bancos de Varejo') %>%
+    mutate(VOL = VAL_FINANCEIRO, Tx = VAL_FINANCEIRO * TAXA_MEDIA) %>% 
+    group_by(DAT_REGISTRO) %>% 
+    summarise(VOL_OC = sum(VOL)) %>% 
+    collect() -> oc_prop_san
+  
+  full_oc_prop_mkt %>% 
+    full_join(oc_prop_san, by='DAT_REGISTRO') %>% 
+    mutate_all(funs(replace(.,is.na(.),0))) %>% 
+    mutate(VOL_OC = VOL_OC.x - VOL_OC.y) %>% 
+    select(DAT_REGISTRO,VOL_OC) %>% 
+    collect() -> oc_prop_mkt
+  
+  cdb_mkt %>% 
+    full_join(oc_prop_mkt, by='DAT_REGISTRO') %>% 
+    mutate_all(funs(replace(.,is.na(.),0))) %>% 
+    mutate(VOL = VOL_CDB + VOL_OC) %>% 
+    select(DAT_REGISTRO,VOL, VOL_CDB, VOL_OC) %>% 
+    collect() -> tbl_mkt
+  
+  bo_santander %>% 
+    filter(DESC_SUB_PROD %in% c("CDB DI", "CDB DI CORPORATE"), 
+           INST == "Cliente") %>% 
+    group_by(DT_CONT) %>% 
+    summarise(DAT_REGISTRO = DT_CONT,VOL = sum(VOL)) %>% 
+    select(DAT_REGISTRO, VOL) %>% 
+    collect() -> san_bo
+  
+  san_bo %>% 
+    full_join(tbl_mkt, by='DAT_REGISTRO') %>% 
+    mutate(Share = VOL.x / (VOL.y + VOL.x)) %>% 
+    collect() -> hist_share
+  
+  return(hist_share)
+}
+
+make_mktmkt_plot <- function(tbl_mercado, tbl_santander, bo_santander){
+  tbl_mercado %>% 
+    filter(Peer == "Grandes bancos de Varejo",
+           INDICE == "DI",
+           EMISSOR == "Mercado",
+           DETENTOR == "Mercado",
+           DAT_REGISTRO >= '2017-05-02') %>% 
+    collect() %>%
+    unite(COD,CONDICAO_RESGATE,CONDICAO_ESPECIFICA,sep="") %>% 
+    filter(COD %in% c("CONDICAO A MERCADONAO","TEM CONDICAOSIM")) %>% 
+    mutate(taxa = TAXA_MEDIA * VAL_FINANCEIRO_TOTAL) %>% 
+    group_by(DAT_REGISTRO) %>% 
+    summarise(Vol_mkt = sum(VAL_FINANCEIRO_TOTAL), Tx_mkt = sum(taxa)) -> cdb_mercado_m2
+  
+  tbl_santander %>% 
+    filter(Peer == "Grandes bancos de Varejo",
+           INDICE == "DI",
+           EMISSOR == "Mercado",
+           DETENTOR == "Mercado",
+           DAT_REGISTRO >= '2017-05-02') %>% 
+    collect() %>% 
+    unite(COD,CONDICAO_RESGATE,CONDICAO_ESPECIFICA,sep="") %>% 
+    filter(COD %in% c("CONDICAO A MERCADONAO","TEM CONDICAOSIM")) %>% 
+    mutate(taxa = TAXA_MEDIA * VAL_FINANCEIRO_TOTAL) %>% 
+    group_by(DAT_REGISTRO) %>% 
+    summarise(Vol_san = sum(VAL_FINANCEIRO_TOTAL), Tx_san = sum(taxa)) -> cdb_santander_m2
+  
+  cdb_mercado_m2 %>% 
+    full_join(cdb_santander_m2, by = 'DAT_REGISTRO') %>% 
+    mutate(VOL = Vol_mkt - Vol_san, Tx = (Tx_mkt - Tx_san)/(Vol_mkt - Vol_san)) %>% 
+    select(DAT_REGISTRO,VOL,Tx) -> cdb_mkt
+  
+  bo_santander %>% 
+    filter(DESC_SUB_PROD %in% c("CDB DI", "CDB DI CORPORATE"), 
+           INST == "Institucionais",
+           DT_CONT >= '2017-05-02') %>%
+    mutate(Tx = VOL * Per_index) %>% 
+    group_by(DT_CONT) %>%
+    summarise(DAT_REGISTRO = DT_CONT,VOL = sum(VOL), Tx = sum(Tx)) %>% 
+    mutate(Taxa = Tx/VOl) %>% 
+    select(DAT_REGISTRO, VOL, Taxa) %>% 
+    collect() -> san_cdb_mktmkt
+  
+  cdb_mkt %>% 
+    full_join(san_cdb_mktmkt, by = 'DAT_REGISTRO') %>% 
+    mutate(share = (VOL.y / (VOL.x + VOL.y)), delta = Taxa - Tx) %>% 
+    select(DAT_REGISTRO,VOL.y,Taxa,share,delta) %>% 
+    collect() %>% 
+    mutate_all(funs(replace(.,is.na(.),0))) -> mktmkt_plot
+  
+  return(mktmkt_plot)
+}
